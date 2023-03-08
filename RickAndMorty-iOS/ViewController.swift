23dc14ee159ab45a85ab8f9b853674da84b74818
Calvin Pak <code@calvinpak.com>
2023-03-08 12:12:55 -0600

    Create a new blank project

---
+++ RickAndMorty-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty-iOS
+//
+//  Created by Calvin Pak on 2023-03-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
