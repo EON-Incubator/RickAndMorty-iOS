14cc3fc452c6375b099afdf55560e49ea83bd40b
Calvin Pak <code@calvinpak.com>
2023-03-08 12:25:56 -0600

    Remove Storyboard.

---
+++ RickAndMorty-iOS/CharactersViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CharactersViewController.swift
+//  RickAndMorty-iOS
+//
+//  Created by Calvin Pak on 2023-03-08.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func loadView() {
+        view = CharactersGridView()
+    }
+
+    
+
+
+}
