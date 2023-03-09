dbff159d5f78c883a0dfb2e7fddd475dcc67424b
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-09 14:17:03 -0600

    Add CharacterImage and it's constraints

--- RickAndMorty-iOS/Views/Characters/CharactersGridCell.swift
+++ RickAndMorty-iOS/Views/Characters/CharactersGridCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import SDWebImage
 
 class CharactersGridCell: UICollectionViewCell {
 
@@ -17,7 +18,16 @@ class CharactersGridCell: UICollectionViewCell {
         label.text = "Name"
         label.textAlignment = .center
         label.backgroundColor = .gray
+        label.layer.opacity = 0.9
         return label
+    }()
+
+    lazy var characterImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.sd_setImage(with: URL(string: "https://picsum.photos/200/300"))
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = self.layer.cornerRadius
+        return imageView
     }()
 
     override init(frame: CGRect) {
@@ -29,10 +39,15 @@ class CharactersGridCell: UICollectionViewCell {
     func setupViews() {
         self.backgroundColor = .lightGray
         self.layer.cornerRadius = 10
+        self.addSubview(characterImage)
         self.addSubview(characterNameLabel)
     }
 
     func setupConstraints() {
+        characterImage.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+
         characterNameLabel.snp.makeConstraints { make in
             make.height.equalTo(30)
             make.left.equalToSuperview()
