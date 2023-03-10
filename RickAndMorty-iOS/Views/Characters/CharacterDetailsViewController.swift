6bede9bd44143cbd259fe4d89e252715afa74b2b
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-10 13:22:09 -0600

    Add CharacterDetailsViewController

---
+++ RickAndMorty-iOS/Views/Characters/CharacterDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CharacterDetailsViewController.swift
+//  RickAndMorty-iOS
+//
+//  Created by Gagan on 2023-03-10.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+
+    weak var coordinator: MainCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
