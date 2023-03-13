89a8fa72d1c0ee5f7369142b974dae65d9fec5e3
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-13 17:43:04 -0500

    Add CharacterDetailsViewModel

--- RickAndMorty-iOS/ViewModel/Characters/CharacterDetailsViewModel.swift
+++ RickAndMorty-iOS/ViewModel/Characters/CharacterDetailsViewModel.swift
@@ -5,4 +5,32 @@
 //  Created by Gagan on 2023-03-13.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+class CharacterDetailsViewModel {
+
+    var character = PassthroughSubject<RickAndMortyAPI.GetCharacterQuery.Data.Character, Never>()
+
+    var selectedCharacter = "" {
+        didSet {
+            fetchData(charID: selectedCharacter)
+        }
+    }
+
+    func fetchData(charID: String) {
+        Network.shared.apollo.fetch(
+            query: RickAndMortyAPI.GetCharacterQuery(characterId: charID)) { result in
+
+                switch result {
+                case .success(let response):
+                    if let char = response.data?.character {
+                        self.character.send(char)
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+
+            }
+    }
+}
