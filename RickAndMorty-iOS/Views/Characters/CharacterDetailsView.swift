ab11dd0876011ea8a5da5982d79387b85233267b
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-12 19:09:17 -0500

    Add CompositionalLayout as collectionViewLayout

--- RickAndMorty-iOS/Views/Characters/CharacterDetailsView.swift
+++ RickAndMorty-iOS/Views/Characters/CharacterDetailsView.swift
@@ -6,15 +6,97 @@
 //
 
 import UIKit
+import SnapKit
+
+enum Section: Int, CaseIterable {
+    case single
+    case info
+    var columnCount: Int {
+        switch self {
+        case .single:
+            return 1
+        case .info:
+            return 3
+        }
+    }
+}
 
 class CharacterDetailsView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
-    */
 
+    init() {
+        super.init(frame: .zero)
+        setupViews()
+        setupConstraints()
+    }
+
+    private func setupViews() {
+        self.backgroundColor = .systemBackground
+        self.addSubview(collectionView)
+        configureDataSource()
+    }
+
+    private func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(self.safeAreaLayoutGuide).inset(UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30))
+        }
+    }
+
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Int>
+    private var dataSource: DataSource!
+
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: self.bounds, collectionViewLayout: createLayout())
+        collectionView.register(CharacterDetailsViewAvatarCell.self,
+                                forCellWithReuseIdentifier: CharacterDetailsViewAvatarCell.identifier)
+
+        return collectionView
+    }()
+
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, _) -> NSCollectionLayoutSection? in
+
+            guard let sectionType = Section(rawValue: sectionIndex) else {
+                return nil
+            }
+
+            let columns = sectionType.columnCount
+
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+
+            let groupHeight = columns == 1 ? NSCollectionLayoutDimension.absolute(310) : NSCollectionLayoutDimension.fractionalWidth(0.50)
+
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: columns)
+
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
+        return layout
+    }
+
+    private func configureDataSource() {
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, _ item) -> UICollectionViewCell? in
+            if indexPath.section == 0 {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterDetailsViewAvatarCell.identifier, for: indexPath)
+                return cell
+            } else {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterDetailsViewAvatarCell.identifier, for: indexPath)
+                return cell
+            }
+        })
+
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.single, .info])
+        snapshot.appendItems([1], toSection: .single)
+        snapshot.appendItems(Array(2...4), toSection: .info)
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
 }
