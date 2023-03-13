e96da8cf51c280ad00430a6c3cfb104d0db4d24f
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-13 10:39:00 -0500

    Add CharacterDetailsUISpec

--- UITests/Characters/CharacterDetailsUISpec.swift
+++ UITests/Characters/CharacterDetailsUISpec.swift
@@ -5,4 +5,40 @@
 //  Created by Gagan on 2023-03-13.
 //
 
-import Foundation
+import XCTest
+import Quick
+import Nimble
+
+final class CharacterDetailsUISpec: QuickSpec {
+
+    override func spec() {
+
+        let app = XCUIApplication()
+
+        describe("Given app launch") {
+
+            app.launch()
+
+            context("when user tap the Character cell on Characters screen") {
+
+                beforeEach {
+                    DispatchQueue.main.async {
+                        app.tabBars.buttons["Characters"].tap()
+                        app.collectionViews.element.cells.element(boundBy: 0).tap()
+
+                    }
+                }
+
+                it("should show the Character details screen") {
+                    let collectionView = app.collectionViews.element
+                    await expect(collectionView.identifier).toEventually(equal("CharacterDetailsView"))
+                }
+
+                it("should display a list of Character Details with at least 1 cell") {
+                    let collectionView = app.collectionViews.element
+                    await expect(collectionView.cells.count).toEventually(beGreaterThanOrEqualTo(1))
+                }
+            }
+        }
+    }
+}
