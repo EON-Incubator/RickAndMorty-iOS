b126f1fae34ab155d9f55f5f11bba78276047a91
Calvin Pak <pak@me.com>
2023-03-13 16:38:03 -0500

    Add example code for fetching single character in DemoViewModel

--- RickAndMorty-iOS/Views/Demo/DemoViewModel.swift
+++ RickAndMorty-iOS/Views/Demo/DemoViewModel.swift
@@ -10,11 +10,28 @@ import Combine
 
 class DemoViewModel {
 
+    var character = PassthroughSubject<RickAndMortyAPI.GetCharacterQuery.Data.Character, Never>()
     var characters = CurrentValueSubject<[RickAndMortyAPI.CharacterBasics], Never>([])
     var currentPage = 0 {
         didSet {
+            fetchCharacter()
             fetchData(page: currentPage)
         }
+    }
+
+    func fetchCharacter() {
+        Network.shared.apollo.fetch(query: RickAndMortyAPI.GetCharacterQuery(
+            characterId: "1")) { result in
+                switch result {
+                case .success(let response):
+                    if let char = response.data?.character {
+                        self.character.send(char)
+                    }
+
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
 
     func fetchData(page: Int) {
