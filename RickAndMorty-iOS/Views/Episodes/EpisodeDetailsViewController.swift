ccfcdfd2f131d71bd54b39b0eb7ba8101a95e43f
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-14 16:06:09 -0500

    Add EpisodeDetailsViewController

---
+++ RickAndMorty-iOS/Views/Episodes/EpisodeDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeDetailsViewController.swift
+//  RickAndMorty-iOS
+//
+//  Created by Gagan on 2023-03-14.
+//
+
+import UIKit
+
+class EpisodeDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
