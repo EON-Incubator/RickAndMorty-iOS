934a36ddfc9ae1ead49d3de0f0bca01933aa2c53
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-14 14:49:36 -0500

    Add EpisodesViewController

---
+++ RickAndMorty-iOS/Views/Episodes/EpisodesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EpisodesViewController.swift
+//  RickAndMorty-iOS
+//
+//  Created by Gagan on 2023-03-14.
+//
+
+import UIKit
+import Combine
+
+class EpisodesViewController: UIViewController {
+
+//    let episodesView = EpisodesView()
+//    let viewModel = EpisodesViewModel()
+    weak var coordinator: MainCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func loadView() {
+//        view = episodesView
+    }
+
+}
