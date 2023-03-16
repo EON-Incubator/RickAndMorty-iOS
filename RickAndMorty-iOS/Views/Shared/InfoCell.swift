d949fb3623c01eef8766aef1ffb3e61d5cc8dc3e
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-16 08:56:53 -0500

    Fix labels text-color for dark-mode

--- RickAndMorty-iOS/Views/Shared/InfoCell.swift
+++ RickAndMorty-iOS/Views/Shared/InfoCell.swift
@@ -30,6 +30,7 @@ class InfoCell: UICollectionViewListCell {
     lazy var leftLabel: UILabel = {
         let label = UILabel()
         label.text = "Gender"
+        label.textColor = .black
         label.font = .boldSystemFont(ofSize: 15)
         return label
     }()
@@ -37,6 +38,7 @@ class InfoCell: UICollectionViewListCell {
     lazy var rightLabel: UILabel = {
         let label = UILabel()
         label.text = "Male"
+        label.textColor = .black
         label.adjustsFontSizeToFitWidth = true
         label.textAlignment = .right
         label.minimumScaleFactor = 0.5
