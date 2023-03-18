e1266ee82c424cdf737c6e0116f51c43007c0ddd
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-17 21:58:05 -0500

    Add viewModel for search characters by name

--- RickAndMorty-iOS/ViewModel/Search/SearchViewModel.swift
+++ RickAndMorty-iOS/ViewModel/Search/SearchViewModel.swift
@@ -6,6 +6,32 @@
 //
 
 import Foundation
-
+import Combine
 class SearchViewModel {
+
+    var characters = CurrentValueSubject<[RickAndMortyAPI.SearchForQuery.Data.Characters.Result], Never>([])
+
+    var locationName = CurrentValueSubject<[RickAndMortyAPI.SearchForQuery.Data.LocationsWithName.Result], Never>([])
+
+    var locationType = CurrentValueSubject<[RickAndMortyAPI.SearchForQuery.Data.LocationsWithType.Result], Never>([])
+
+    var searchInput = "" {
+        didSet {
+            fetchData(input: searchInput)
+        }
+    }
+
+    func fetchData(input: String) {
+        Network.shared.apollo.fetch(
+            query: RickAndMortyAPI.SearchForQuery(keyword: GraphQLNullable<String>(stringLiteral: input))) { result in
+                    switch result {
+                    case .success(let response):
+
+                        self.characters.value = (response.data?.characters?.results?.compactMap { $0 })!
+
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+    }
 }
