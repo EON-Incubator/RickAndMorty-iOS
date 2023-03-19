b013e6b416bc3c8f02da93b444747ac1343383ee
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-03-19 12:28:00 -0500

    Add logic to search characters by name and location-type

--- RickAndMorty-iOS/Views/Search/SearchView.swift
+++ RickAndMorty-iOS/Views/Search/SearchView.swift
@@ -8,4 +8,93 @@
 import UIKit
 
 class SearchView: UIView {
+
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: self.bounds, collectionViewLayout: createLayout())
+        collectionView.register(CharacterRowCell.self,
+                                forCellWithReuseIdentifier: CharacterRowCell.identifier)
+        return collectionView
+    }()
+
+    var locationCell = UICollectionView.CellRegistration<RowCell, RickAndMortyAPI.SearchForQuery.Data.LocationsWithName.Result> { (cell, _ indexPath, location) in
+        cell.contentView.backgroundColor = UIColor(red: 0.93, green: 0.95, blue: 1.00, alpha: 0.5)
+
+        cell.lowerRightLabel.backgroundColor = UIColor(red: 1.00,
+                                                  green: 0.92,
+                                                  blue: 0.71,
+                                                  alpha: 0.4)
+        cell.lowerRightLabel.layer.borderWidth = 0.3
+        cell.lowerRightLabel.layer.borderColor = UIColor.gray.cgColor
+
+        cell.lowerLeftLabel.layer.borderWidth = 0.3
+        cell.lowerLeftLabel.layer.borderColor = UIColor.gray.cgColor
+        cell.lowerLeftLabel.backgroundColor = UIColor(red: 1.00,
+                                                 green: 0.75,
+                                                 blue: 0.66,
+                                                 alpha: 0.4)
+        cell.upperLabel.text = location.name
+        cell.lowerLeftLabel.text = location.type
+        cell.lowerRightLabel.text = location.dimension
+
+        for index in 0...3 {
+            let isIndexValid = location.residents.indices.contains(index)
+            if isIndexValid {
+                let urlString = location.residents[index]?.image ?? ""
+                cell.characterAvatarImageViews[index].sd_setImage(with: URL(string: urlString))
+            }
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    init() {
+        super.init(frame: .zero)
+        collectionView.showsVerticalScrollIndicator = false
+        setupViews()
+        setupConstraints()
+    }
+
+    private func setupViews() {
+        self.backgroundColor = .systemBackground
+        self.addSubview(collectionView)
+    }
+
+    private func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(self.safeAreaLayoutGuide).inset(UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10))
+        }
+    }
+
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, _) -> NSCollectionLayoutSection? in
+
+            guard let sectionType = Section(rawValue: sectionIndex) else {
+                return nil
+            }
+
+            let columns = sectionType.columnCount
+
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+
+            let groupHeight = NSCollectionLayoutDimension.estimated(100)
+
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, repeatingSubitem: item, count: columns)
+
+            let section = NSCollectionLayoutSection(group: group)
+
+            return section
+        }
+        return layout
+    }
 }
--- RickAndMorty-iOS/Views/Search/SearchViewController.swift
+++ RickAndMorty-iOS/Views/Search/SearchViewController.swift
@@ -6,15 +6,76 @@
 //
 
 import UIKit
+import Combine
 
 class SearchViewController: UIViewController {
+
+    enum SearchSection: Int, CaseIterable {
+        case characters
+        case locationsWithName
+        case locationsWithType
+    }
 
     let searchView = SearchView()
     let viewModel = SearchViewModel()
     weak var coordinator: MainCoordinator?
 
+    typealias DataSource = UICollectionViewDiffableDataSource<SearchSection, AnyHashable>
+    private var dataSource: DataSource!
+    private var cancellables = Set<AnyCancellable>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        configureDataSource()
+        subscribeToViewModel()
+        viewModel.searchInput = "planet"
+    }
+
+    override func loadView() {
+        view = searchView
+    }
+
+    func subscribeToViewModel() {
+        viewModel.searchResults.sink(receiveValue: { result in
+            var snapshot = NSDiffableDataSourceSnapshot<SearchSection, AnyHashable>()
+            snapshot.appendSections([.locationsWithName, .characters])
+            snapshot.appendItems((result.locationsWithName?.results)!, toSection: .locationsWithName)
+            snapshot.appendItems((result.characters?.results)!, toSection: .characters)
+
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }).store(in: &cancellables)
+    }
+
+    private func configureDataSource() {
+        dataSource = DataSource(collectionView: searchView.collectionView, cellProvider: { (collectionView, indexPath, result) -> UICollectionViewCell? in
+
+            var cell = UICollectionViewCell()
+
+            switch indexPath.section {
+            case 0:
+                cell = collectionView.dequeueConfiguredReusableCell(using: self.searchView.locationCell, for: indexPath, item: result as? RickAndMortyAPI.SearchForQuery.Data.LocationsWithName.Result)
+
+            case 1:
+                if let character = result as? RickAndMortyAPI.SearchForQuery.Data.Characters.Result {
+
+                    let characterRowCell = (collectionView.dequeueReusableCell(withReuseIdentifier: CharacterRowCell.identifier, for: indexPath) as? CharacterRowCell)!
+
+                    let urlString = character.image ?? ""
+                    characterRowCell.characterAvatarImageView.sd_setImage(with: URL(string: urlString))
+                    characterRowCell.upperLabel.text = character.name
+                    characterRowCell.lowerLeftLabel.text = character.gender
+                    characterRowCell.lowerRightLabel.text = character.species
+                    characterRowCell.characterStatusLabel.text = character.status
+                    characterRowCell.characterStatusLabel.backgroundColor = characterRowCell.statusColor(character.status ?? "")
+
+                    cell = characterRowCell
+                }
+            default:
+                cell = UICollectionViewCell()
+            }
+            return cell
+        })
     }
 
 }
