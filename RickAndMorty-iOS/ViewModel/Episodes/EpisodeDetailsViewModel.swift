4411bd85fdcaa894091488207a7cfd7d24a463ee
Calvin Pak <pak@me.com>
2023-04-19 14:18:37 -0500

    Fix an issue where Play Video button does not appear in Offline Mode even there is Internet Connection

--- RickAndMorty-iOS/ViewModel/Episodes/EpisodeDetailsViewModel.swift
+++ RickAndMorty-iOS/ViewModel/Episodes/EpisodeDetailsViewModel.swift
@@ -128,6 +128,7 @@ class EpisodeDetailsViewModel {
     func getDataFromDB() {
         if let epi = Network.shared.getEpisode(episodeId: episodeId) {
             self.episode.send(epi)
+            self.episodeVideo = epi.episodeDetails?.episodeVideos.first?.key
         }
     }
 
